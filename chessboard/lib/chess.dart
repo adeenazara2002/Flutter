0350352549d698964ed4404a46735fd06cc4f5c4
adeenazara2002 <adinazara2001@gmail.com>
2024-06-03 10:54:36 +0500

    Chess board

---
+++ chessboard/lib/chess.dart
@@ -0,0 +1,327 @@
+import 'package:flutter/material.dart';
+
+class ChessBoard extends StatelessWidget {
+  const ChessBoard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor:Colors.cyan,
+      title: Text('Chess Board'),
+      // tit: Text('CHESS BOARD'),
+),
+
+body:Column(
+  children: [
+    Row(
+      children: [
+        Container(
+          color: Colors.black,
+          height: 60,
+          width: 50,
+        ),
+
+Container(
+          color: Colors.pink,
+          height: 60,
+          width: 50,
+        ),
+        
+        
+        Container(
+          color: Colors.black,
+          height: 60,
+          width: 50,
+        ),
+        
+        
+        Container(
+          color: Colors.pink,
+          height: 60,
+          width: 50,
+        ),
+        
+        Container(
+          color: Colors.black,
+          height: 60,
+          width: 50,
+        ),
+        
+        Container(
+          color: Colors.pink,
+          height: 60,
+          width: 50,
+        ),
+
+        Container(
+          color: Colors.black,
+          height: 60,
+          width: 50,
+        ),
+        
+            ],
+    ),
+
+
+
+
+    Row(
+      children: [
+        Container(
+          color: Colors.pink,
+          height: 60,
+          width: 50,
+        ),
+
+Container(
+          color: Colors.black,
+          height: 60,
+          width: 50,
+        ),
+        
+        
+        Container(
+          color: Colors.pink,
+          height: 60,
+          width: 50,
+        ),
+        
+        
+        Container(
+          color: Colors.black,
+          height: 60,
+          width: 50,
+        ),
+        
+        Container(
+          color: Colors.pink,
+          height: 60,
+          width: 50,
+        ),
+        
+        Container(
+          color: Colors.black,
+          height: 60,
+          width: 50,
+        ),
+
+        Container(
+          color: Colors.pink,
+          height: 60,
+          width: 50,
+        ),
+        
+            ],
+    ),
+
+
+
+    Row(
+      children: [
+        Container(
+          color: Colors.black,
+          height: 60,
+          width: 50,
+        ),
+
+Container(
+          color: Colors.pink,
+          height: 60,
+          width: 50,
+        ),
+        
+        
+        Container(
+          color: Colors.black,
+          height: 60,
+          width: 50,
+        ),
+        
+        
+        Container(
+          color: Colors.pink,
+          height: 60,
+          width: 50,
+        ),
+        
+        Container(
+          color: Colors.black,
+          height: 60,
+          width: 50,
+        ),
+        
+        Container(
+          color: Colors.pink,
+          height: 60,
+          width: 50,
+        ),
+
+        Container(
+          color: Colors.black,
+          height: 60,
+          width: 50,
+        ),
+        
+            ],
+    ),
+
+
+
+    
+    Row(
+      children: [
+        Container(
+          color: Colors.pink,
+          height: 60,
+          width: 50,
+        ),
+
+Container(
+          color: Colors.black,
+          height: 60,
+          width: 50,
+        ),
+        
+        
+        Container(
+          color: Colors.pink,
+          height: 60,
+          width: 50,
+        ),
+        
+        
+        Container(
+          color: Colors.black,
+          height: 60,
+          width: 50,
+        ),
+        
+        Container(
+          color: Colors.pink,
+          height: 60,
+          width: 50,
+        ),
+        
+        Container(
+          color: Colors.black,
+          height: 60,
+          width: 50,
+        ),
+
+        Container(
+          color: Colors.pink,
+          height: 60,
+          width: 50,
+        ),
+        
+            ],
+    ),
+
+
+
+    Row(
+      children: [
+        Container(
+          color: Colors.black,
+          height: 60,
+          width: 50,
+        ),
+
+Container(
+          color: Colors.pink,
+          height: 60,
+          width: 50,
+        ),
+        
+        
+        Container(
+          color: Colors.black,
+          height: 60,
+          width: 50,
+        ),
+        
+        
+        Container(
+          color: Colors.pink,
+          height: 60,
+          width: 50,
+        ),
+        
+        Container(
+          color: Colors.black,
+          height: 60,
+          width: 50,
+        ),
+        
+        Container(
+          color: Colors.pink,
+          height: 60,
+          width: 50,
+        ),
+
+        Container(
+          color: Colors.black,
+          height: 60,
+          width: 50,
+        ),
+        
+            ],
+    ),
+
+
+
+    Row(
+      children: [
+        Container(
+          color: Colors.pink,
+          height: 60,
+          width: 50,
+        ),
+
+Container(
+          color: Colors.black,
+          height: 60,
+          width: 50,
+        ),
+        
+        
+        Container(
+          color: Colors.pink,
+          height: 60,
+          width: 50,
+        ),
+        
+        
+        Container(
+          color: Colors.black,
+          height: 60,
+          width: 50,
+        ),
+        
+        Container(
+          color: Colors.pink,
+          height: 60,
+          width: 50,
+        ),
+        
+        Container(
+          color: Colors.black,
+          height: 60,
+          width: 50,
+        ),
+
+        Container(
+          color: Colors.pink,
+          height: 60,
+          width: 50,
+        ),
+        
+            ],
+    ),
+  ],
+  
+),   
+
+    );
+  }
+}
